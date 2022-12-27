ea90c40a0eed911119ccf36253b6ce7a6af3a654
Code <xiazhongyu2000@gmail.com>
2022-12-27 22:50:47 +0800

    commit

--- 02_Python入门语法/08_Python字符串的多种定义方式.py
+++ 02_Python入门语法/08_Python字符串的多种定义方式.py
@@ -18,3 +18,17 @@ name = """"
 程序员
 """
 print(type(name))
+
+# 在字符串内 包含双引号
+name = '"黑马程序员"'
+print(name)
+# 在字符串内 包含单引号
+name = "'黑马程序员'"
+print(name)
+# 使用转义字符\解除引号的效用
+name = "\"黑马程序员\""
+print(name)
+name = '\'黑马程序员\''
+print(name)
+
+print("\\")
