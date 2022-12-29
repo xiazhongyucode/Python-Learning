68118aaee30e09acd9f59e92d4b4a779ac895373
Code <xiazhongyu2000@gmail.com>
2022-12-29 21:07:39 +0800

    commit

--- 02_Python入门语法/13_字符串格式化练习题.py
+++ 02_Python入门语法/13_字符串格式化练习题.py
@@ -13,4 +13,4 @@ finally_stock_price = stock_price * stock_price_daily_growth_factor ** growth_da
 
 print(f"公司：{name}，股票代码：{stock_code}，当前股价：{stock_price}")
 print("每日增长系数：%.1f，经过%d天的增长后，股价达到了：%.2f" % (
-stock_price_daily_growth_factor, growth_days, finally_stock_price))
+    stock_price_daily_growth_factor, growth_days, finally_stock_price))
--- 02_Python入门语法/14_input语句.py
+++ 02_Python入门语法/14_input语句.py
@@ -9,4 +9,4 @@ print("我知道了，你是：%s" % name)
 num = input("请告诉我你的银行卡密码：")
 # 数据类型转换
 num = int(num)
-print("你的银行卡密码的类型是：",type(num))
+print("你的银行卡密码的类型是：", type(num))
