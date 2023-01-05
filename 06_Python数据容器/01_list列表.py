da2ae0db7bae05a04ec7ebc396bbf41b4e657d15
Code <xiazhongyu2000@gmail.com>
2023-01-06 00:09:32 +0800

    commit

---
+++ 06_Python数据容器/01_list列表.py
@@ -0,0 +1,24 @@
+"""
+演示数据容器之：list列表
+语法：[元素, 元素,......]
+"""
+# 定义一个列表list
+my_list = ["itheima", "itcast", "python"]
+print(my_list)
+print(type(my_list))
+
+my_list = ["itheima", 666, True]
+print(my_list)
+print(type(my_list))
+
+# 定义一个嵌套的列表
+my_list = [[1, 2, 3], [4, 5, 6]]
+print(my_list)
+print(type(my_list))
+
+# 通过下标索引取出对应位置的数据
+
+
+# 通过下标索引取出数据（倒序取出）
+
+# 取出嵌套列表的元素
