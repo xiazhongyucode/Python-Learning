575bb7769108eb506887a9a3044971b5aa4b8121
Code <xiazhongyu2000@gmail.com>
2023-01-18 18:08:16 +0800

    commit

--- 10_Python基础综合案例/02_pyecharts基础入门.py
+++ 10_Python基础综合案例/02_pyecharts基础入门.py
@@ -3,12 +3,20 @@
 """
 # 导包
 from pyecharts.charts import Line
+from pyecharts.options import TitleOpts,LegendOpts,ToolboxOpts,VisualMapOpts
+
 # 创建一个折线图对象
 line = Line()
 # 给折线图对象添加x轴的数据
-line.add_xaxis(["中国","美国","英国"])
+line.add_xaxis(["中国", "美国", "英国"])
 # 给折线图对象添加y轴的数据
-line.add_yaxis("GDP",[30,20,10])
+line.add_yaxis("GDP", [30, 20, 10])
+# 设置全局配置项
+line.set_global_opts(
+    title_opts=TitleOpts(title="GDP展示",pos_left="center",pos_bottom="1%"),
+    legend_opts=LegendOpts(is_show=True),
+    toolbox_opts=ToolboxOpts(is_show=True),
+    visualmap_opts=VisualMapOpts(is_show=True),
+)
 # 通过render方法，将代码生成为图像
 line.render()
-# 设置全局配置项
