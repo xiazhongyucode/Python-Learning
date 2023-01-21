9d41fa8ae618271c31fa5d666c3168b6dbbae7d7
Code <xiazhongyu2000@gmail.com>
2023-01-21 20:14:02 +0800

    commit

---
+++ 10_Python可视化案例/07_基础柱状图开发.py
@@ -0,0 +1,17 @@
+"""
+演示基础柱状图的开发
+"""
+from pyecharts.charts import Bar
+from pyecharts.options import LabelOpts
+
+# 使用Bar构建基础柱状图
+bar = Bar()
+# 添加x轴的数据
+bar.add_xaxis(["中国", "美国", "英国"])
+# 添加y轴的数据
+bar.add_yaxis("GDP", [30, 20, 10],label_opts=LabelOpts(position="right"))
+# 反转xy轴
+bar.reversal_axis()
+# 绘图
+bar.render("基础柱状图.html")
+# 设置数值标签在右侧
