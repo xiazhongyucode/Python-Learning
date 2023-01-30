43abd05326eb31b20344461fd92beed2fe2cabad
Code <xiazhongyu2000@gmail.com>
2023-01-30 20:26:12 +0800

    commit

---
+++ 13_pyspark大数据分析/03_数据计算_map方法.py
@@ -0,0 +1,27 @@
+"""
+演示RDD的map成员方法的使用
+"""
+import os
+
+from pyspark import SparkConf, SparkContext
+
+os.environ['PYSPARK_PYTHON'] = "C:/Program Files/Python311/python.exe"
+
+conf = SparkConf().setMaster("local[*]").setAppName("test_spark")
+sc = SparkContext(conf=conf)
+
+# 准备一个RDD
+rdd = sc.parallelize([1, 2, 3, 4, 5])
+
+# 通过map方法将全部数据都乘以10
+# def func(data):
+#     return data * 10
+#
+#
+# rdd2 = rdd.map(func)
+rdd2 = rdd.map(lambda x: x * 10).map(lambda x: x + 5)
+
+print(rdd2.collect())
+# (T) -> U
+# (T) -> T
+# 链式调用
